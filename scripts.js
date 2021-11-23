8b9f5dfbf9debf58edbe2a259e6953a49e9367cc
Pat Kelly <PatK80@gmail.com>
2021-11-22 20:43:11 -0500

    using params in axios

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@ const form = document.querySelector("#search-form");
 form.addEventListener("submit", async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
-    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    const config = { params: {q: searchTerm} }
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
     makeImages(res.data);
 });
 
